199545acb07ccd2ec68bd0f8092ceb85ad3d44d9
shanuka <shanuka.jayarathna.work@gmail.com>
2026-05-06 18:06:11 +0530

    feat(dashboard): route horoscope seekers to specialized dashboard

--- src/features/dashboard/pages/DashboardRouter.tsx
+++ src/features/dashboard/pages/DashboardRouter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 import { RootState } from '@/app/store/store';
 import UserDashboard from './UserDashboard';
 import CoupleDashboard from './CoupleDashboard';
@@ -11,6 +12,10 @@ import CoupleDashboard from './CoupleDashboard';
  */
 export default function DashboardRouter() {
   const { user } = useSelector((state: RootState) => state.auth);
+
+  if (user?.profileType === 'horoscope_seeker') {
+    return <Navigate to="/horoscope" replace />;
+  }
 
   // Determine if user is a couple based on wedding project
   const isCouple = user?.profileType === 'couple' || !!user?.weddingProject?.partnerName;
